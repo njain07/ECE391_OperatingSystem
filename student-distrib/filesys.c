1db114ff0617c0efd2d14ff143a1ba0a836fb454
Vismayak Mohanarajan <vismayak97@gmail.com>
2018-03-27 16:56:23 -0500

    compiler errors taken out

--- student-distrib/filesys.c
+++ student-distrib/filesys.c
@@ -9,11 +9,11 @@
  */
 extern void filesys_init()
 {
-	boot_block->num_dir = FILESYS_ADDR;
-	boot_block->num_inodes = FILESYS_ADDR + FOUR_B;
-	boot_block->num_dblocks = FILESYS_ADDR + EIGHT_B;
-	boot_block->reserved = FILESYS_ADDR + TWELVE_B;
-	boot_block->dentries = FILESYS_ADDR + SF_B;
+	// boot_block->num_dir = FILESYS_ADDR;
+	// boot_block->num_inodes = FILESYS_ADDR + FOUR_B;
+	// boot_block->num_dblocks = FILESYS_ADDR + EIGHT_B;
+	// boot_block->reserved = FILESYS_ADDR + TWELVE_B;
+	// boot_block->dentries = FILESYS_ADDR + SF_B;
 }
 
 /* filesys_open
@@ -78,23 +78,24 @@ int32_t filesys_close(int32_t fd)
  */
 int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry)
 {
-	uint32_t index, num_dentries, retval;
-	retval = -1;
-	num_dentries = boot_block->num_dir;
+	// uint32_t index, num_dentries, retval;
+	// retval = -1;
+	// num_dentries = boot_block->num_dir;
 
-	for(index = 0; index < num_dentries; index++)
-	{
-		if(strncmp(fname, boot_block->dentries[index]->file_name, DENTRY_FILE_NAME_SIZE-1) == 0)
-		{
-			strncpy(dentry->file_name, boot_block->dentries[index]->file_name, DENTRY_FILE_NAME_SIZE-1);
-			dentry->file_name[DENTRY_FILE_NAME_SIZE] = '\0';
-			dentry->file_type = boot_block->dentries[index]->file_type;
-			dentry->inode_num = boot_block->dentries[index]->inode_num;
-			retval = 0;
-		}
-	}
+	// for(index = 0; index < num_dentries; index++)
+	// {
+	// 	if(strncmp(fname, boot_block->dentries[index]->file_name, DENTRY_FILE_NAME_SIZE-1) == 0)
+	// 	{
+	// 		strncpy(dentry->file_name, boot_block->dentries[index]->file_name, DENTRY_FILE_NAME_SIZE-1);
+	// 		dentry->file_name[DENTRY_FILE_NAME_SIZE] = '\0';
+	// 		dentry->file_type = boot_block->dentries[index]->file_type;
+	// 		dentry->inode_num = boot_block->dentries[index]->inode_num;
+	// 		retval = 0;
+	// 	}
+	// }
 
-	return retval;
+	// return retval;
+	return -1;
 }
 
 /* read_dentry_by_index
@@ -107,15 +108,15 @@ int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry)
  */
 int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry)
 {
-	uint32_t num_dentries;
-	num_dentries = boot_block->num_dir;
-	if(index < 0 || index > (num_dentries-1) || dentry == NULL)
-		return -1;
+	// uint32_t num_dentries;
+	// num_dentries = boot_block->num_dir;
+	// if(index < 0 || index > (num_dentries-1) || dentry == NULL)
+	// 	return -1;
 
-	strncpy(dentry->file_name, boot_block->dentries[index]->file_name, DENTRY_FILE_NAME_SIZE-1);
-	dentry->file_name[DENTRY_FILE_NAME_SIZE] = '\0';
-	dentry->file_type = boot_block->dentries[index]->file_type;
-	dentry->inode_num = boot_block->dentries[index]->inode_num;
+	// strncpy(dentry->file_name, boot_block->dentries[index]->file_name, DENTRY_FILE_NAME_SIZE-1);
+	// dentry->file_name[DENTRY_FILE_NAME_SIZE] = '\0';
+	// dentry->file_type = boot_block->dentries[index]->file_type;
+	// dentry->inode_num = boot_block->dentries[index]->inode_num;
 	return 0;
 }
 
