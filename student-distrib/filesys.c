5484ffc16a3f22d88079f60fd24fc0ce987c4441
Jain <njain15@illinois.edu>
2018-03-27 11:48:20 -0500

    saving progress

--- student-distrib/filesys.c
+++ student-distrib/filesys.c
@@ -1,21 +1,41 @@
-#include filesys.h
+#include "filesys.h"
 
 extern void filesys_init()
 {
 
 }
 
+int32_t filesys_open(const uint8_t* filename)
+{
+	return 0;
+}
+
+int32_t filesys_read(int32_t fd, void* buf, int32_t nbytes)
+{
+	return 0;
+}
+
+int32_t filesys_write(int32_t fd, const void* buf, int32_t nbytes)
+{
+	return 0;
+}
+
+int32_t filesys_close(int32_t fd)
+{
+	return 0;
+}
+
 int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry)
 {
-
+	return 0;
 }
 
 int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry)
 {
-
+	return 0;
 }
 
 int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length)
 {
-	
+	return 0;
 }
--- student-distrib/filesys.h
+++ student-distrib/filesys.h
@@ -4,19 +4,19 @@
 #include "types.h" 
 #include "lib.h" 
 
-typedef struct
+typedef struct inode_t
 {
-	
+
 } inode_t;
 
-typedef struct
+typedef struct dentry_t
 {
 	uint8_t file_name[33];
 	uint32_t file_type;
 	uint32_t inode_num;
 } dentry_t;
 
-typedef struct
+typedef struct boot_t
 {
 	uint32_t num_dir;
 	uint32_t num_inodes;
@@ -24,6 +24,12 @@ typedef struct
 } boot_t;
 
 extern void filesys_init();
+
+int32_t filesys_open(const uint8_t* filename);
+int32_t filesys_read(int32_t fd, void* buf, int32_t nbytes);
+int32_t filesys_write(int32_t fd, const void* buf, int32_t nbytes);
+int32_t filesys_close(int32_t fd);
+
 int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
 int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry);
 int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
