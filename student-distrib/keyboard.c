d823c49ffa1413f5111d6f478040fc0345367e03
Jain <njain15@illinois.edu>
2018-04-12 20:31:05 -0500

    all keys working correctly

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -25,12 +25,26 @@ const uint8_t lowercase[59] =
     '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 
     0, 0, 0, ' ', 0};
 
-// const uint8_t shift_case[59] = 
-//     {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0, 
-//     0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 
-//     0, 0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 
-//     '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 
-//     0, 0, 0, ' ', 0};
+const uint8_t shift_case[59] = 
+    {0, 0, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 0, 
+    0, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 
+    0, 0, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '\"', 
+    '~', 0, '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 
+    0, 0, 0, ' ', 0};
+
+const uint8_t caps_case[59] = 
+    {0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0, 
+    0, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', 
+    0, 0, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', 
+    '`', 0, '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 
+    0, 0, 0, ' ', 0};
+
+const uint8_t shift_caps_case[59] = 
+    {0, 0, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 0, 
+    0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', 
+    0, 0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '\"', 
+    '~', 0, '|', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', 
+    0, 0, 0, ' ', 0};
 
 /* Simple init helper for keyboard initialization */
 void keyboard_init(void) {
@@ -43,50 +57,6 @@ void keyboard_init(void) {
 
 /* Read the actual character and return it */
 char get_character(uint8_t scancode){
-    // char return_char;
-    
-    // switch (scancode){
-    //     case 0x0B:    return_char = '0';    break;
-    //     case 0x02:    return_char = '1';    break;
-    //     case 0x03:    return_char = '2';    break;
-    //     case 0x04:    return_char = '3';    break;
-    //     case 0x05:    return_char = '4';    break;
-    //     case 0x06:    return_char = '5';    break;
-    //     case 0x07:    return_char = '6';    break;
-    //     case 0x08:    return_char = '7';    break;
-    //     case 0x09:    return_char = '8';    break;
-    //     case 0x0a:    return_char = '9';    break;
-    //     case 0x1E:    return_char = 'a';    break;
-    //     case 0x30:    return_char = 'b';    break;
-    //     case 0x2E:    return_char = 'c';    break;
-    //     case 0x20:    return_char = 'd';    break;
-    //     case 0x12:    return_char = 'e';    break;
-    //     case 0x21:    return_char = 'f';    break;
-    //     case 0x22:    return_char = 'g';    break;
-    //     case 0x23:    return_char = 'h';    break;
-    //     case 0x17:    return_char = 'i';    break;
-    //     case 0x24:    return_char = 'j';    break;
-    //     case 0x25:    return_char = 'k';    break;
-    //     case 0x26:    return_char = 'l';    break;
-    //     case 0x32:    return_char = 'm';    break;
-    //     case 0x31:    return_char = 'n';    break;
-    //     case 0x18:    return_char = 'o';    break;
-    //     case 0x19:    return_char = 'p';    break;
-    //     case 0x10:    return_char = 'q';    break;
-    //     case 0x13:    return_char = 'r';    break;
-    //     case 0x1F:    return_char = 's';    break;
-    //     case 0x14:    return_char = 't';    break;
-    //     case 0x16:    return_char = 'u';    break;
-    //     case 0x2F:    return_char = 'v';    break;
-    //     case 0x11:    return_char = 'w';    break;
-    //     case 0x2D:    return_char = 'x';    break;
-    //     case 0x15:    return_char = 'y';    break;
-    //     case 0x2C:    return_char = 'z';    break;
-    //     default:      return_char = 'x';    break;
-    // }
-    
-    // return return_char;
-
     //print scancodes to check which set to use
     //handle unknown scancodes
 
@@ -95,62 +65,67 @@ char get_character(uint8_t scancode){
         case 0x2A:  
             // left shift pressed
             flags.shift = 1;
-            printf("left shift pressed | ");   
+            // printf("left shift pressed | ");   
             break;
 
         case 0x36:
             // right shift pressed 
             flags.shift = 1;
-            printf("right shift pressed | ");
+            // printf("right shift pressed | ");
             break;
 
         case 0xAA:
             // left shift released
             flags.shift = 0;
-            printf("left shift released | ");
+            // printf("left shift released | ");
             //return 0;
             break;
 
         case 0xB6:
             // right shift released
             flags.shift = 0;
-            printf("right shift released | ");
+            // printf("right shift released | ");
             break;
 
         case 0x3A:
             // caps
-            flags.caps = ~flags.caps;
-            printf("ALL CAPS | ");
+            flags.caps = !(flags.caps);
+            // printf("ALL CAPS | ");
             break;
 
         case 0x1D:
             // left or right ctrl pressed
             flags.ctrl = 1;
-            printf("ctrl pressed | ");
+            // printf("ctrl pressed | ");
             break;
 
         case 0x9D:
             // left or right ctrl released
             flags.ctrl = 0;
-            printf("ctrl released | ");
+            // printf("ctrl released | ");
             break;
 
         case 0x38:
             // left or right alt pressed
             flags.alt = 1;
-            printf("alt pressed | ");
+            // printf("alt pressed | ");
             break;
 
         case 0xB8:
             // left or right alt released
             flags.alt = 0;
-            printf("alt released | ");
+            // printf("alt released | ");
             break;
     }
 
     if(flags.ctrl == 1)
     {
         //check for ctrl-l
+        if(scancode == 0x26 || scancode == 0xA6)
+        {
+            clear();
+            return 0;
+        }
     }
 
     if(flags.shift == 0 && flags.caps == 0 && flags.alt == 0)
@@ -161,22 +136,25 @@ char get_character(uint8_t scancode){
 
     else if(flags.shift == 1 && flags.caps == 0 && flags.alt == 0)
     {
-
+        if(scancode < 0x3B)
+            return shift_case[scancode];
     }
 
     else if(flags.shift == 0 && flags.caps == 1 && flags.alt == 0)
     {
-
+        if(scancode < 0x3B)
+            return caps_case[scancode];
     }
 
     else if(flags.shift == 1 && flags.caps == 1 && flags.alt == 0)
     {
-
+        if(scancode < 0x3B)
+            return shift_caps_case[scancode];
     }
     
     else if(flags.alt == 1)
     {
-        //not able to type
+        return 0;
     }
 
     return 0;
@@ -195,19 +173,16 @@ void keyboard_interrupt_handler(void){
     if(inb(KEYBOARD_STATUS_PORT))
     {
         scancode = inb(KEYBOARD_PORT);
-        //if(scancode > 0 && scancode < 0x3B)
-        //{
-            /* interpret the character */
-            print_char = get_character(scancode);
-            
-            /* putting into keyboard buffer */
-            if(buffer_index < BUFFER_SIZE-1)
-                buffer[buffer_index++] = print_char;
-            
-            /* echo to the screen */
-            if(print_char!=0)
-               putc(print_char);
-        //}
+        /* interpret the character */
+        print_char = get_character(scancode);
+        
+        /* putting into keyboard buffer */
+        if(buffer_index < BUFFER_SIZE-1)
+            buffer[buffer_index++] = print_char;
+        
+        /* echo to the screen */
+        if(print_char!=0)
+           putc(print_char);
     }
 
     /* critical section ended */
