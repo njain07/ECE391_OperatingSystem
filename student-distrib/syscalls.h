ed607322ebfcacabb3d12acdc359694ccc5af7cc
Jain <njain15@illinois.edu>
2018-04-14 11:25:33 -0500

    adding syscalls files

---
+++ student-distrib/syscalls.h
@@ -0,0 +1,22 @@
+#ifndef _SYSCALLS_H
+#define _SYSCALLS_H
+
+#include "lib.h"
+#include "interrupts.S"
+#include "filesys.h"
+#include "rtc.h"
+#include "paging.h"
+#include "pcb.h"
+
+int32_t execute(const uint8_t* command);
+int32_t halt(uint8_t* status);
+int32_t open(const uint8_t* filename);
+int32_t read(int32_t fd, void* buf, int32_t nbytes);
+int32_t write(int32_t fd, const void* buf, int32_t nbytes);
+int32_t close(int32_t fd);
+int32_t getargs(uint8_t* buf, int32_t nbytes);
+int32_t vidmap(uint8_t** screen_start);
+int32_t set_handler(int32_t signum, void* handler_address);
+int32_t sigreturn(void);
+
+#endif
