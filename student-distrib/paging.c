c5c249b699c4d7f9dcb24ecc76fc95d0dd562e67
Nikita <njain15@illinois.edu>
2018-04-27 13:31:20 -0500

    fish works

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -80,9 +80,9 @@ void process_page(int32_t pid)
 void vidmap_page(uint8_t** screen_start)
 {
 
-	video_memory[0] = VIDEO_MEM | U_W_PRESENT;
-	page_directory[33] = (uint32_t)video_memory | U_W_PRESENT;
-	// flush_TLB();
+	video_memory[0] = VIDEO_MEM | 0x07; //U_W_PRESENT;
+	page_directory[33] = (uint32_t)video_memory | 0x07; //U_W_PRESENT;
+	flush_TLB();
 
 }
 
